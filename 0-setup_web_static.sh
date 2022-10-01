24d2a8edfd121c3207d7f8501f1ca0db0ac74c3c
melakualehegn <melakualehegn34@gmail.com>
2022-10-01 20:06:34 +0300

    Task-0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 #a Bash script that sets up your web servers for the deployment of web_static
-sudo apt-get upgrade
-sudo apt-get install nginx
-sudo mkdir -p /data/web_static/shared /data/web_static/releases/test
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+	Hello world
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "23i location \/hbnb_static {\nalias /data/web_static/current;\n}" /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
