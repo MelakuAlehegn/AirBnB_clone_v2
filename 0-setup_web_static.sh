587ed0d5d8437a59cbc5a2f3fec7eb0a821f15e6
melakualehegn <melakualehegn34@gmail.com>
2022-10-01 20:04:09 +0300

    Task-0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,5 +6,5 @@ sudo mkdir -p /data/web_static/shared /data/web_static/releases/test
 echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "38i location \/hbnb_static {\nalias /data/web_static/current;\n}" /etc/nginx/sites-available/default
+sudo sed -i "23i location \/hbnb_static {\nalias /data/web_static/current;\n}" /etc/nginx/sites-available/default
 sudo service nginx restart
