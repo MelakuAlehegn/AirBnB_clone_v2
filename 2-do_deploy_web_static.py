f42258682f95801771b711475a5ee330333fda7f
melakualehegn <melakualehegn34@gmail.com>
2022-10-02 21:53:07 +0300

    Task-1:fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,28 +10,28 @@ import os.path
 
 
 env.hosts = ['3.236.253.64', '18.207.158.241']
+
+
 def do_deploy(archive_path):
     '''
     a function to deploy an archive
     '''
+
     path = os.path.exists(archive_path)
-    if not path:
-        return False
-    else:
-        try:
-            file_path = archive_path.split("/")[-1]
-            without_ext = file_path.split(".")[0]
-            path = "/data/web_static/releases/"
-            put(archive_path, '/tmp/')
-            run('mkdir -p {}{}/'.format(path, without_ext))
-            run('tar -xzf /tmp/{} -C {}{}/'.format(file_path, path, without_ext))
-            run('rm /tmp/{}'.format(file_path))
-            run('mv {0}{1}/web_static/* {0}{1}/'.format(path, without_ext))
-            run('rm -rf {}{}/web_static'.format(path, without_ext))
-            run('rm -rf /data/web_static/current')
-            run('ln -s {}{}/ /data/web_static/current'.format(path, without_ext))
-            
-            print("New version deployed!")
-            return True
-        except:
-            return False
+    if path:
+        file_path = archive_path.split("/")[-1]
+        without_ext = file_path.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, without_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_path, path, without_ext))
+        run('rm /tmp/{}'.format(file_path))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, without_ext))
+        run('rm -rf {}{}/web_static'.format(path, without_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, without_ext))
+
+        print("New version deployed!")
+        return True
+
+    return False
