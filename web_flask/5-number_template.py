edc9cb568d9911d28dd42563f804e377b20b2b11
melakualehegn <melakualehegn34@gmail.com>
2022-10-17 00:11:47 +0300

    Task-5:fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def nisnum(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     '''display a HTML page only if n is an integer:'''
-    return render_template('5-number.html',n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
