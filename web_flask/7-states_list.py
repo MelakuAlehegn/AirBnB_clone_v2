1a0e2ef15e1abeed33aa89ac7ff5f94e4628dcce
melakualehegn <melakualehegn34@gmail.com>
2022-10-20 23:18:57 +0300

    Task-8:fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,8 +16,9 @@ def Teardown():
 @app.route('/states_list/<>', strict_slashes=False)
 def states_list():
     '''display a HTML page for statest list'''
-    state = sorted(list(storage.all("States").values()), key= lambda x: x.name)
+    state = sorted(list(storage.all("States").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', state=state)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
