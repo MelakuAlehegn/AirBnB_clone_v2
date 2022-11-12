5d1b226da973673b79dfd5c61410116d7bb52988
melakualehegn <melakualehegn34@gmail.com>
2022-11-12 18:40:13 +0000

    Task-0:application server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     '''index page'''
     return 'Hello HBNB!'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
